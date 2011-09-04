03ad557df74cac6b05e53e4fc4ef17cdffacca8a
Oleksandr Ulianytskyi <a.ulyanitsky@gmail.com>
2011-09-04 13:45:21 +0300

    Added deployment mode option for `rsm:bunde:install`

--- lib/tasks/bundle/install.rb
+++ lib/tasks/bundle/install.rb
@@ -1,10 +1,13 @@
 module Rsm
   module Bundle
     class Install < Rsm::Base
+      class_option :deployment, :aliases => "-D", :type => :boolean, :default => false, :desc => "Use deployment mode"
 
       def check_and_install
+        bundle_install = "bundle install"
+        bundle_install << " --deployment" if options[:deployment]
         inside "." do
-          run_ruby_binary "bundle check || bundle install"
+          run_ruby_binary "bundle check || #{bundle_install}"
         end
       end
 
